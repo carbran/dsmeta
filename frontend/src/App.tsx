1cfad912a535c01998cf5d2747ae71659cb0066c
Fernando Brandão <77928944+carbran@users.noreply.github.com>
2022-07-13 23:27:39 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
+  return (
+    <> {/*Fragment - usado quando se tem mais de um elemento*/}
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
